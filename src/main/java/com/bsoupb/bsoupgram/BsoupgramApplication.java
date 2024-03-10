29363f46062ea796ba2422f68f978f5b46072e01
LAPTOP-BTT3NSUQ\qotn0 <qotn0327@naver.com>
2024-03-10 22:12:18 +0900

    [배수빈] - bsoupgram 환경 설정

---
+++ src/main/java/com/bsoupb/bsoupgram/BsoupgramApplication.java
@@ -0,0 +1,13 @@
+package com.bsoupb.bsoupgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsoupgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsoupgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsoupb/bsoupgram/BsoupgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsoupb.bsoupgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsoupgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
